42275403fd785d2dcbae21e09f47f15fa1861e5c
Matt Mendez Zugenbuehler <mzugenbuehler@gmail.com>
2025-03-06 18:22:34 -0800

    edits for qa

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router' // add import for BrowserRouter
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
 
 // Wrap the App component with the BrowserRouter component to enable
 // enable route handling throughout your application.
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
